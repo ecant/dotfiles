2d287f4b5f2d7c17f30680583f30b3776ed0342d
Eryn Cantrell <eryn.cantrell@thumbraise.com>
2019-05-19 15:34:36 -0500

    More updates

--- zshrc.zsh
+++ zshrc.zsh
@@ -36,3 +36,7 @@ ZSH_CUSTOM="$HOME/dotfiles/zsh-custom"
 plugins=(git z history npm vscode extract common-aliases zsh-dircolors-solarized zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
