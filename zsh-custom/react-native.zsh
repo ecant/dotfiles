a73335f8cf72d60619beab2ba8b0a370343e82a5
Eryn Cantrell <eryn.cantrell@thumbraise.com>
2019-06-03 15:55:37 -0500

    updates for fastlane

--- zsh-custom/react-native.zsh
+++ zsh-custom/react-native.zsh
@@ -3,4 +3,6 @@ export PATH="$PATH:$ANDROID_HOME/emulator"
 export PATH="$PATH:$ANDROID_HOME/tools"
 export PATH="$PATH:$ANDROID_HOME/tools/bin"
 export PATH="$PATH:$ANDROID_HOME/platform-tools"
-export PATH="$PATH:$HOME/.fastlane/bin"
+
+export LC_ALL=en_US.UTF-8   # for fastlane
+export LANG=en_US.UTF-8     # for fastlane
